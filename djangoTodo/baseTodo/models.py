3a7f0f32033feec14b2e7d35ff97334017a59101
Jeffry Chavenia <chaveniajeffry19@gmail.com>
2023-05-09 22:59:41 +0800

    update models.py

--- djangoTodo/baseTodo/models.py
+++ djangoTodo/baseTodo/models.py
@@ -8,3 +8,16 @@ class Todo(models.Model):
     status = models.BooleanField(default=False)
     when_added = models.PositiveIntegerField(default=int(timezone.now().timestamp()))
     when_updated = models.PositiveIntegerField(blank=True, null=True)
+    
+    class Meta:
+        db_name = 'todo'
+        
+class RecentActivity(models.Model):
+    who_added = models.PositiveIntegerField()
+    when_added = models.PositiveIntegerField()
+    action = models.CharField(max_length=255)
+    page_id = models.PositiveIntegerField()
+    obj_id = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = 'recent_activity'
