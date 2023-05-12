b0ce00fb3278d3f9c0bf73ebce48337bfd6dd15a
Jeffry Chavenia <chaveniajeffry19@gmail.com>
2023-05-12 23:48:06 +0800

    update views.py

--- djangoTodo/baseTodo/views.py
+++ djangoTodo/baseTodo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+from .models import Todo, RecentActivity
 def home(request):
     return HttpResponse("Hello, world. You're at the base todo home.")
 
@@ -9,7 +9,11 @@ def home(request):
 def createTodo(request):
     pass
 def readTodo(request):
-    pass
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos
+    }
+    return render(request, "base/home.html",context)
 def updateTodo(request):
     pass
 def deleteTodo(request):
