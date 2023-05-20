bc706285fe6a89e2af16210b8126c8d8690b52a1
Jeffry Chavenia <chaveniajeffry19@gmail.com>
2023-05-20 23:23:37 +0800

    update settings.py

--- djangoTodo/djangoTodo/settings.py
+++ djangoTodo/djangoTodo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'djangoTodo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'baseTodo/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,6 +65,8 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"),]
 
 WSGI_APPLICATION = 'djangoTodo.wsgi.application'
 
