3342b91b25747c7528fe7bd96c66311f3d2a3566
Jeffry Chavenia <chaveniajeffry19@gmail.com>
2023-06-28 23:30:34 +0800

    add levels in task count to be shown in calendar

--- djangoTodo/static/js/script.js
+++ djangoTodo/static/js/script.js
@@ -22,79 +22,34 @@ date_length.forEach((item, index) => {
     formatted_search_query_start_date = new_month + "/" + new_day + "/" + new_start_date.getFullYear()
     item.textContent = formatted_search_query_start_date + " " + time
 });
-// document.addEventListener('DOMContentLoaded', function() {
-//   var calendarDiv = document.getElementById("calendar");
+function sumByDate(tableData) {
+  const sumsByDate = {};
 
-//   // Create table for each month
-//   for (var month = 0; month < 12; month++) {
-//     var table = document.createElement('table');
+for (let i = 0; i < tableData.length; i++) {
+  const date = tableData[i];
 
-//     // Create table header (month and weekdays)
-//     var monthName = new Date(new Date().getFullYear(), month).toLocaleString('default', { month: 'long' });
-//     var headerRow = document.createElement('tr');
-//     var monthHeader = document.createElement('th');
-//     monthHeader.setAttribute('colspan', '7');
-//     monthHeader.textContent = monthName;
-//     headerRow.appendChild(monthHeader);
-//     table.appendChild(headerRow);
+  if (sumsByDate[date]) {
+    sumsByDate[date]++;
+  } else {
+    sumsByDate[date] = 1;
+  }
+}
 
-//     var weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-//     var weekdaysRow = document.createElement('tr');
-//     weekdays.forEach(function(day) {
-//       var th = document.createElement('th');
-//       th.appendChild(document.createTextNode(day));
-//       weekdaysRow.appendChild(th);
-//     });
-//     table.appendChild(weekdaysRow);
+return sumsByDate;
+}
 
-//     // Create table cells (dates)
-//     var currentDate = new Date();
-//     var today = currentDate
-//     var currentYear = new Date().getFullYear();
-//     var firstDay = new Date(currentYear, month, 1);
-//     var lastDay = new Date(currentYear, month + 1, 0);
+// Example usage:
+let tableData = []
+all_dates = document.querySelectorAll(".local-date-format")
+all_dates.forEach(element => {
+  date_format = new Date(element.textContent)
+  tableData.push(date_format.toLocaleDateString())
+});
+const result = sumByDate(tableData);
 
-//     var startDate = new Date(firstDay);
-//     startDate.setDate(firstDay.getDate() - firstDay.getDay()); // Set start date to Sunday of the first week
-
-//     var endDate = new Date(lastDay);
-//     endDate.setDate(lastDay.getDate() + (6 - lastDay.getDay())); // Set end date to Saturday of the last week
-
-//     var currentDate = new Date(startDate);
-
-//     while (currentDate <= endDate) {
-//       var row = document.createElement('tr');
-
-//       for (var i = 0; i < 7; i++) {
-//         var cell = document.createElement('td');
-
-//         if (currentDate.getMonth() == month) {
-//           cell.appendChild(document.createTextNode(currentDate.getDate()));
-//           date_length = document.querySelectorAll(".local-date-format")
-//           date_length.forEach((item, index) => {
-//             let new_start_date = new Date(item.textContent)
-            
-//               if (new_start_date.getDate() == currentDate.getDate() && new_start_date.getMonth() == currentDate.getMonth()) {
-//                 cell.id= "one"
-//               }
-//           })
-//           if(currentDate.getDate() == today.getDate() && currentDate.getMonth() == today.getMonth()){
-//             cell.id= "today"
-//           }
-//         }
-
-//         row.appendChild(cell);
-//         currentDate.setDate(currentDate.getDate() + 1);
-//       }
-
-//       table.appendChild(row);
-//     }
-
-//     // Append the table to the calendar div
-//     calendarDiv.appendChild(table);
-//   }
-// });
-
+// Convert result object to array of [date, count] pairs
+const data = Object.entries(result);
+// This is for the calendar
 document.addEventListener('DOMContentLoaded', function() {
   var calendarDiv = document.getElementById("calendar");
 
@@ -119,14 +74,17 @@ document.addEventListener('DOMContentLoaded', function() {
       var headerRow = document.createElement('tr');
       var monthHeader = document.createElement('th');
       monthHeader.setAttribute('colspan', '7');
+      monthHeader.classList.add('month-header');
       monthHeader.textContent = monthName;
       headerRow.appendChild(monthHeader);
       table.appendChild(headerRow);
 
       var weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
       var weekdaysRow = document.createElement('tr');
+      // weekdays.classList.add('week-days')
       weekdays.forEach(function(day) {
         var th = document.createElement('th');
+        th.classList.add('weekdays')
         th.appendChild(document.createTextNode(day));
         weekdaysRow.appendChild(th);
       });
@@ -154,17 +112,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
           if (currentDate.getMonth() == monthCount) {
             cell.appendChild(document.createTextNode(currentDate.getDate()));
-            date_length = document.querySelectorAll(".local-date-format")
-            date_length.forEach((item, index) => {
-              let new_start_date = new Date(item.textContent)
+            let date_series = data.map(([date, count]) => [date, count]);
+            date_series.forEach((item,index)=>{
               
-                if (new_start_date.getDate() == currentDate.getDate() && new_start_date.getMonth() == currentDate.getMonth()) {
-                  cell.id= "one"
+              // ['6/20/2023', 4] item
+              let new_date = new Date(item[0])
+                
+                if (new_date.getDate() == currentDate.getDate() && new_date.getMonth() == currentDate.getMonth() && (item[1] <=5)) {
+                  cell.classList.add('task-level-one')
+                  console.log("item",item[0])
+                  console.log("item",item[1])
+                }
+                else if (new_date.getDate() == currentDate.getDate() && new_date.getMonth() == currentDate.getMonth() && (item[1] >=6 && item[1] <=10)) {
+                  cell.classList.add('task-level-two')
+                  console.log("item",item[0])
+                  console.log("item",item[1])
+                }
+                else if (new_date.getDate() == currentDate.getDate() && new_date.getMonth() == currentDate.getMonth() && (item[1] >=11 && item[1] <=15)) {
+                  cell.classList.add('task-level-three')
+                  console.log("item",item[0])
+                  console.log("item",item[1])
+
+                }
+                else if (new_date.getDate() == currentDate.getDate() && new_date.getMonth() == currentDate.getMonth() && (item[1] >15)) {
+                  cell.classList.add('task-level-four')
+                  console.log("item",item[0])
+                  console.log("item",item[1])
+
                 }
             })
-            if(currentDate.getDate() == today.getDate() && currentDate.getMonth() == today.getMonth()){
-              cell.id= "today"
-            }
           }
 
           row.appendChild(cell);
@@ -184,4 +160,35 @@ document.addEventListener('DOMContentLoaded', function() {
   // Append the calendar container to the calendar div
   calendarDiv.appendChild(yearContainer);
   calendarDiv.appendChild(calendarContainer);
+});
+
+
+// Prepare the data for Highcharts series
+const seriesData = data.map(([date, count]) => [Date.parse(date), count]);
+
+// Create the Highcharts chart
+Highcharts.chart('chartContainer', {
+chart: {
+  type: 'area'
+},
+title: {
+  text: 'Date Counts'
+},
+xAxis: {
+  type: 'datetime',
+  title: {
+    text: 'Date'
+  }
+},
+yAxis: {
+  title: {
+    text: 'Count'
+  }
+},
+series: [{
+  name: 'Count',
+  data: seriesData,
+  pointStart: Date.parse(tableData[0].split('T')[0]), // Specify the starting date of the x-axis
+  pointInterval: 24 * 3600 * 1000 // Set the interval to 1 day (24 hours)
+}]
 });
